8389b397e365530d8b9c916781bab4af1e9c8865
khyeborg <kbl274@nyu.edu>
2020-06-28 03:22:53 -0400

    first commit

--- main.js
+++ main.js
@@ -7,71 +7,76 @@ for (let i = 0; i < 17; i++) {
 	bigArray.push([]);
 }
 
-$(document).ready(function() {
+function all() {
 
-  // query the PokeAPI for info about this pokemon
-  $.ajax({
-    url: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=649',
-    success: function(data, status) {
-      console.log(data);
+  $(document).ready(function() {
 
-      for (let i = 0; i < 650; i++) {
+    // query the PokeAPI for info about this pokemon
+    $.ajax({
+      url: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=649',
+      success: function(data, status) {
+        console.log(data);
 
-        checkImage("footprints/" + (i + 1) + ".png", 
-          // if image loads
-          function() { 
-            let name = data.results[i].name;
-            let nameLength = name.length; // 19 is the longest
+        for (let i = 0; i < 650; i++) {
 
-            bigArray[nameLength - 3].push([name, i + 1]);
-          
-            if (i == 648) {
-              let bigDiv = document.createElement("div");
+          checkImage("footprints/" + (i + 1) + ".png", 
+            // if image loads
+            function() { 
+              let name = data.results[i].name;
+              let nameLength = name.length; // 19 is the longest
 
-              for (let j = 0; j < bigArray.length - 9; j++) {
-                let newTable = document.createElement("table");
-                let tr = document.createElement("tr");
+              bigArray[nameLength - 3].push([name, i + 1]);
+            
+              if (i == 648) {
+                let bigDiv = document.createElement("div");
 
-                tr.innerHTML = (j + 3) + " letters";
+                for (let j = 0; j < bigArray.length - 9; j++) {
+                  let newTable = document.createElement("table");
+                  let tr = document.createElement("tr");
 
-                newTable.appendChild(tr);
+                  tr.innerHTML = (j + 3) + " letters";
 
-                for (let k = 0; k < bigArray[j].length; k++) {
-                  if (bigArray[j][k][0].indexOf("-") == -1 && bigArray[j][k][0].indexOf("shellos") == -1 && bigArray[j][k][0].indexOf("gastrodon") == -1) {
-                    let tr2 = document.createElement("tr");
-                    let td1 = document.createElement("td");
-                    let td2 = document.createElement("td");
-                    let newImg = document.createElement("img");
+                  newTable.appendChild(tr);
 
-                    newImg.src = "footprints/" + bigArray[j][k][1] + ".png";
-                    td1.innerHTML = bigArray[j][k][0];
+                  for (let k = 0; k < bigArray[j].length; k++) {
+                    if (bigArray[j][k][0].indexOf("-") == -1 && bigArray[j][k][0].indexOf("shellos") == -1 && bigArray[j][k][0].indexOf("gastrodon") == -1) {
+                      let tr2 = document.createElement("tr");
+                      let td1 = document.createElement("td");
+                      let td2 = document.createElement("td");
+                      let newImg = document.createElement("img");
 
-                    newTable.appendChild(tr2);
-                    tr2.appendChild(td1);
-                    tr2.appendChild(newImg);
-                  }
+                      newImg.src = "footprints/" + bigArray[j][k][1] + ".png";
+                      td1.innerHTML = bigArray[j][k][0];
+
+                      newTable.appendChild(tr2);
+                      tr2.appendChild(td1);
+                      tr2.appendChild(newImg);
+                    }
+                }
+
+                bigDiv.appendChild(newTable);
+                }
+
+                body.appendChild(bigDiv);
               }
+            }, 
 
-              bigDiv.appendChild(newTable);
-              }
+            // if image does not load
+            function() { } );
 
-              body.appendChild(bigDiv);
-            }
-          }, 
+        }
+      },
 
-          // if image does not load
-          function() { } );
-
+      error: function(req, data, status) {
+        console.log("error");
       }
-    },
 
-    error: function(req, data, status) {
-      console.log("error");
-    }
+    });
 
-  });
+  }); // end jQuery document ready function
+}
 
-}); // end jQuery document ready function
+setTimeout(all, 5000);
 
 function checkImage(imageSrc, good, bad) {
     var img = new Image();
